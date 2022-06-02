80ccebd08dfbd80c3fe58e7326aa0c0bbb7eb819
Mirek <miro.zaprazny8@gmail.com>
2022-06-02 12:35:39 +0200

    admin post  blog section

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,15 +8,22 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index () {
-        return view('posts.index',[
-            'posts' =>  Post::latest()->filter(request(['search','category','user']))->paginate()->withQueryString()
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' =>  Post::latest()->filter(request(['search', 'category', 'user']))->paginate()->withQueryString()
         ]);
     }
 
-    public function show (Post $post){
+    public function show(Post $post)
+    {
         return view('posts.show', [
             'post' => $post,
-            ]);
+        ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
     }
 }
